eaf129af5bec9309c14a68d63dfea3d5672acfed
Josue Ibarra <jibarr21@ext.uber.com>
2024-03-12 17:10:18 -0700

    remove outdated test, ensure slack link works

--- cypress/integration/slack/slack-short-link.spec.js
+++ cypress/integration/slack/slack-short-link.spec.js
@@ -3,11 +3,6 @@ context('Slack short link', () => {
     cy.visit('http://t.uber.com/cadence-slack');
   });
 
-  it('should render the page with at least an email input.', () => {
-    cy.get('input[name="email"]')
-      .should('be.visible');
-  });
-
   it('should redirect to the join URL', () => {
     cy.location().should(({ host, pathname }) => {
       expect(host).to.equal('uber-cadence.slack.com');
